79637560f0186fa38183c8599aa6bd30f41c9139
arpit92k <arpit92k@gmail.com>
2015-03-23 22:08:27 +0530

    final commint with java compile support

--- src/main/mainClass.java
+++ src/main/mainClass.java
@@ -3,10 +3,22 @@ package main;
 public class mainClass {
 
 	public static void main(String[] args) {
-		if(args.length==1)
-			new ExecCode(args[0],false);
-		else
-			System.err.println("Invalid use refer documentation");
+		if(args[0].equals("-v")){
+			if(args.length==2)
+				new ExecCode(args[1],false);
+			else if(args.length==3)
+				new ExecCode(args[1],args[2],false);
+			else
+				System.err.println("Invalid use refer documentation");
+		}
+		else{
+			if(args.length==1)
+				new ExecCode(args[0]);
+			else if(args.length==2)
+				new ExecCode(args[0],args[1],true);
+			else
+				System.err.println("Invalid use refer documentation");
+		}
 	}
 
 }
